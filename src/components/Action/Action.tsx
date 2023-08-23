66d97b5924f43d80e14e4f01f9b03c934c592f3b
whyiwant <youyuanchong@gmail.com>
2023-08-23 17:20:35 +0800

    update: update connect page

---
+++ src/components/Action/Action.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+export default function Action(props: any) {
+  return (
+    <>
+      <div className="relative mb-8 mt-8 flex w-2/3 items-center rounded-lg bg-white text-black">
+        <div className="mb-8 ms-8 mt-8 text-4xl">{props.title}</div>
+
+        <AiOutlineArrowRight className="absolute right-0 me-10 text-4xl"></AiOutlineArrowRight>
+      </div>
+    </>
+  );
+}
--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Action from "~/components/Action/Action";
 
 export default function connect() {
+  const options = [
+    {
+      title: "参与小组",
+      url: "To be develop",
+    },
+    {
+      title: "参与服事",
+      url: "To be develop",
+    },
+    {
+      title: "洗礼",
+      url: "To be develop",
+    },
+  ];
   return (
-    <div className="h-screen flex flex-col justify-center items-center">connect</div>
-  )
+    <>
+      <div className="mb-8 mt-8 flex flex-col items-center">
+        {options.map((opt, index) => {
+          return <Action title={opt.title} url={opt.url}></Action>;
+        })}
+      </div>
+    </>
+  );
 }
