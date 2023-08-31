d377910df01445eb27d5f03f2247d53af9a663d9
whyiwant <youyuanchong@gmail.com>
2023-09-01 01:37:28 +0800

    fix: fix bugs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 import { nextui } from "@nextui-org/react";
-import { nextui } from "@nextui-org/react";
 import { type Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
